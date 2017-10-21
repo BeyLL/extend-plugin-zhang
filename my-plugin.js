d7ce3ebb8dc61b6cf0e0f31c60604ac8daff1987
zhangweiheng <zhangheng@upvi.com>
2017-10-21 22:13:35 +0800

    init

---
+++ my-plugin.js
@@ -0,0 +1,16 @@
+/**
+ * Created by "zhangHeng" on 2017/10/21 0021.
+ */
+(function($){
+    $.fn.extend({
+        choice:function(options){
+            var defaults = {  //初始化defaults对象
+
+            };
+            var opt = $.extend({},defaults,options)
+        }
+    })
+
+
+
+})(jQuery);
